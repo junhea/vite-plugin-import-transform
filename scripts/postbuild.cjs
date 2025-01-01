ffc6833aa3cbb3fc8505b12f9438c2967b7a03b8
junhea <97426534+junhea@users.noreply.github.com>
2025-01-01 20:17:46 +0900

    set type & update postbuild script to prevent mistakenly publishing project

--- scripts/postbuild.cjs
+++ scripts/postbuild.cjs
@@ -1,5 +1,14 @@
 const fs = require("fs")
 fs.copyFileSync("./src/types.d.ts", "./dist/types.d.ts")
-fs.copyFileSync("./package.json", "./dist/package.json")
 fs.copyFileSync("./README.md", "./dist/README.md")
 fs.copyFileSync("./LICENSE", "./dist/LICENSE")
+
+const packageJson = JSON.parse(fs.readFileSync("./package.json"))
+packageJson.private = false
+packageJson.scripts = undefined
+packageJson.prettier = undefined
+
+fs.writeFileSync(
+  "./dist/package.json",
+  JSON.stringify(packageJson, undefined, 2),
+)
